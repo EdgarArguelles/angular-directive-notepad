fcd002af419e086aae153fb9487e658c439dc50e
edgar <ing.edgar.arguelles@gmail.com>
2015-06-30 13:03:45 -0500

    improve functionality

--- src/app/app.js
+++ src/app/app.js
@@ -9,20 +9,25 @@
             scope: {},
             templateUrl: 'app/notepad.tpl.html',
             link: function (scope, elem, attrs) {
-                scope.openEditor = function (index) {
+                scope.delete = function (id) {
+                    scope.notes = notesFactory.delete(id);
+                    scope.restore();
+                };
+                scope.openEditor = function (id) {
                     scope.editMode = true;
-                    if (index !== undefined) {
-                        scope.noteText = notesFactory.get(index).content;
-                        scope.index = index;
-                    } else
+                    if (id !== undefined) {
+                        scope.noteText = notesFactory.get(id).content;
+                        scope.index = id;
+                    } else {
                         scope.noteText = undefined;
+                    }
                 };
                 scope.save = function () {
                     if (scope.noteText !== "" && scope.noteText !== undefined) {
                         var note = {};
                         note.title = scope.noteText.length > 10 ? scope.noteText.substring(0, 10) + '. . .' : scope.noteText;
                         note.content = scope.noteText;
-                        note.id = scope.index != -1 ? scope.index : localStorage.length;
+                        note.id = scope.index != -1 ? scope.index : Math.random();;
                         scope.notes = notesFactory.put(note);
                     }
                     scope.restore();
@@ -51,12 +56,16 @@
 
     function notesFactory() {
         return {
+            delete: function (id) {
+                localStorage.removeItem('note' + id);
+                return this.getAll();
+            },
             put: function (note) {
                 localStorage.setItem('note' + note.id, JSON.stringify(note));
                 return this.getAll();
             },
-            get: function (index) {
-                return JSON.parse(localStorage.getItem('note' + index));
+            get: function (id) {
+                return JSON.parse(localStorage.getItem('note' + id));
             },
             getAll: function () {
                 var notes = [];
